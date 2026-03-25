7694135b721b8646df8ebbf9658ecaf1068ea77c
Puneet Jain <puneet120802@gmail.com>
2026-03-25 23:14:13 +0530

    Fix Apple Silicon adbd crash and disable auto-reset

--- adbd_helper/jni/main.cpp
+++ adbd_helper/jni/main.cpp
@@ -1,17 +1,18 @@
 #include <unistd.h>
 
+#include <stdlib.h>
+
 int main(int argc, char** argv) {
     const char* real_adbd = "/apex/com.android.adbd/bin/adbd.real";
 
-    char* const envp[] = {
-        const_cast<char*>("LD_PRELOAD=/system/lib64/libadb_root_helper.so"),
-    };
+    setenv("LD_PRELOAD", "/system/lib64/libadb_root_helper.so", 1);
 
     // u:r:adbd:s0 is restricted
     char* const new_argv[] = { 
         argv[0],
-        const_cast<char*>("--root_seclabel=u:r:magisk:s0")
+        const_cast<char*>("--root_seclabel=u:r:magisk:s0"),
+        nullptr
     };
 
-    return execve(real_adbd, new_argv, envp);
+    return execv(real_adbd, new_argv);
 }
