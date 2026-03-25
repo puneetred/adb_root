aed2261ef84c60720c2aee4bf4675a95ba922313
Puneet Jain <puneet120802@gmail.com>
2026-03-25 22:03:12 +0530

    Fix adbd crash on Android 12 by using setenv and execv with properly terminated arrays

--- adbd_helper/jni/main.cpp
+++ adbd_helper/jni/main.cpp
@@ -1,28 +1,16 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
     const char* real_adbd = "/apex/com.android.adbd/bin/adbd.real";
 
-    char* const envp[] = {
-        const_cast<char*>("LD_PRELOAD=/system/lib64/libadb_root_helper.so"),
-    };
+    setenv("LD_PRELOAD", "/system/lib64/libadb_root_helper.so", 1);
 
-    // 'su' domain is usually permissive in Magisk/KernelSU
+    // u:r:adbd:s0 is restricted
     char* const new_argv[] = { 
-        argv[0],
-        const_cast<char*>("--root_seclabel=u:r:su:s0")
+        (argc > 0) ? argv[0] : const_cast<char*>("adbd"),
+        const_cast<char*>("--root_seclabel=u:r:magisk:s0"),
+        nullptr
     };
 
-    int ret = execve(real_adbd, new_argv, envp);
-    
-    // If we reach here, execve failed
-    FILE* kmsg = fopen("/dev/kmsg", "w");
-    if (kmsg) {
-        fprintf(kmsg, "adb_root: execve failed: %s (%d) for path %s\n", strerror(errno), errno, real_adbd);
-        fclose(kmsg);
-    }
-    return ret;
+    return execv(real_adbd, new_argv);
 }
