5a6667afb2685696632e052486344511e59f820f
Puneet Jain <puneet120802@gmail.com>
2026-03-25 22:10:41 +0530

    Fix SELinux denials for adb shell commands on Android 12

--- adbd_helper/jni/main.cpp
+++ adbd_helper/jni/main.cpp
@@ -1,16 +1,17 @@
 #include <unistd.h>
-#include <stdlib.h>
+
 int main(int argc, char** argv) {
     const char* real_adbd = "/apex/com.android.adbd/bin/adbd.real";
 
-    setenv("LD_PRELOAD", "/system/lib64/libadb_root_helper.so", 1);
+    char* const envp[] = {
+        const_cast<char*>("LD_PRELOAD=/system/lib64/libadb_root_helper.so"),
+    };
 
     // u:r:adbd:s0 is restricted
     char* const new_argv[] = { 
-        (argc > 0) ? argv[0] : const_cast<char*>("adbd"),
-        const_cast<char*>("--root_seclabel=u:r:magisk:s0"),
-        nullptr
+        argv[0],
+        const_cast<char*>("--root_seclabel=u:r:magisk:s0")
     };
 
-    return execv(real_adbd, new_argv);
+    return execve(real_adbd, new_argv, envp);
 }
